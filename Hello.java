8abf5ec8ac046200c4a286174e72c09705d78be3
rasel <mdraselmiah1374@gmail.com>
2022-11-01 20:07:34 +0600

    Added a new method called display to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
       public static void main(string[]args) {
         system.out.printIn{"vs code is awesome..."};
+
+        greeting{"rasel"};
+      }
+      public static void greeting(sting name).{
+            system.out.printIn{"wellcome " +  name};
       }
 }
